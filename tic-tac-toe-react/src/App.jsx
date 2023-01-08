e0e605038645a67fc45aa3d5b801eae8dd2018d7
Arthur Valladares Nole <arthurvalladares@belcorp.biz>
2023-01-08 18:50:43 -0500

    Put base board and square components

--- tic-tac-toe-react/src/App.jsx
+++ tic-tac-toe-react/src/App.jsx
@@ -1,6 +1,44 @@
-import { useState } from 'react'
+import { useState, Fragment } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+
+function Square({ value, onSquareClick }) {
+  return (
+    <button className="square" onClick={onSquareClick}>
+      {value}
+    </button>
+  );
+}
+
+function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
+  function handleClick(i) {
+    const nextSquares = squares.slice();
+    nextSquares[i] = "X";
+    setSquares(nextSquares);
+  }
+
+  return (
+    <Fragment>
+      <div className="board-row">
+        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
+      </div>
+      <div className="board-row">
+        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
+      </div>
+      <div className="board-row">
+        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
+      </div>
+    </Fragment>
+  );
+}
 
 function App() {
 
@@ -11,6 +49,7 @@ function App() {
         <a href="https://reactjs.org" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
+        <Board></Board>
       </div>
     </div>
   )
