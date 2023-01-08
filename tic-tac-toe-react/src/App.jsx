25b0e4ec8bd79b916666874b2ffd0e631b2d179f
Arthur Valladares Nole <arthurvalladares@belcorp.biz>
2023-01-08 16:38:41 -0500

    init commit

---
+++ tic-tac-toe-react/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <div>
+        <h1>Tic Tac Toe</h1>
+        <a href="https://reactjs.org" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+    </div>
+  )
+}
+
+export default App
